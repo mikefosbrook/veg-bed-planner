5392f98668c3f87537fc7c7776d7d77f807b5d45
Mike Fosbrook <michael.fosbrook@and.digital>
2023-06-11 00:51:06 +0100

    Rendering bed data from mocked data via JSON Server
    - Removed default bed data
    - Removed createCells function that generated cell data on the fly
    - Bed only renders if there is data
    - Added error handling
    - Added loading status
    - Set css for body tag to place items at top of page to prevent jumping between re-renders

--- src/components/Bed.jsx
+++ src/components/Bed.jsx
@@ -1,45 +1,49 @@
 import { useState, useEffect } from 'react';
 
 export default function Bed() {
-  const defaultBed = {
-    id: 1,
-    name: 'Bed 1',
-    cellsX: 6,
-    cellsY: 4,
-  };
-  const [bedDetails, setBedDetails] = useState(defaultBed);
-  const [bedCells, setBedCells] = useState([]);
-
-  function createCells(x, y) {
-    const cells = x * y;
-    const emptyCells = [];
-    for (let i = 1; i <= cells; i++) {
-      let cell = {
-        id: i,
-        name: `Cell '${i}`,
-      };
-      emptyCells.push(cell);
-    }
-    return emptyCells;
-  }
+  const [bedDetails, setBedDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    setBedCells(createCells(bedDetails.cellsX, bedDetails.cellsY));
-  }, [bedDetails]);
-
-  const styles = { gridTemplateColumns: 'auto '.repeat(bedDetails.cellsX) };
+    fetch('http://localhost:4000/beds/1')
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data for that resource');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBedDetails(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, []);
 
   return (
     <>
-      <h2>{bedDetails.name}</h2>
-      <p>
-        {bedDetails.cellsX} x {bedDetails.cellsY}
-      </p>
-      <div className="bed" style={styles}>
-        {bedCells.map((cell, i) => {
-          return <div className="cell" key={i}></div>;
-        })}
-      </div>
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+
+      {bedDetails && (
+        <>
+          <h2>{bedDetails.name}</h2>
+
+          <p>
+            {bedDetails.cellsX} x {bedDetails.cellsY}
+          </p>
+
+          <div className="bed" style={{ gridTemplateColumns: 'auto '.repeat(bedDetails.cellsX) }}>
+            {bedDetails.cells.map((cell, i) => {
+              return <div className="cell" key={i}></div>;
+            })}
+          </div>
+        </>
+      )}
     </>
   );
 }
