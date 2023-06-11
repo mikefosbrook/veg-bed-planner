fff54a622ff9697e39a603b46e11c9030e317103
Mike Fosbrook <michael.fosbrook@and.digital>
2023-06-11 10:36:52 +0100

    Preventing error incase fetch is in progress and user navigates to another page before the fetch completes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,7 +5,8 @@ const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController();
+    fetch(url, { signal: abortCont.signal })
       .then((res) => {
         if (!res.ok) {
           throw Error('Could not fetch the data for that resource');
@@ -18,9 +19,15 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((err) => {
-        setIsLoading(false);
-        setError(err.message);
+        if (err.name === 'AbortError') {
+          console.log('Fetch request was cancelled');
+        } else {
+          setIsLoading(false);
+          setError(err.message);
+        }
       });
+
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isLoading, error };
