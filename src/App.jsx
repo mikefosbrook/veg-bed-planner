265fd9a48864aa69a1c2abcdfc60212ec0b03010
Mike Fosbrook <michael.fosbrook@and.digital>
2023-06-11 08:56:34 +0100

    Implementing routes using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
 import './App.scss';
 import Bed from './components/Bed.jsx';
 import CreateBed from './components/CreateBed';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <h1>Veg bed planner</h1>
-      {/* <Bed /> */}
-      <CreateBed />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CreateBed />} />
+          <Route path="/beds/:id" element={<Bed />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
