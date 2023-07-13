6b029fc058cb954479b860c44a08831f10c2997d
Mike Fosbrook <mikefosbrook@yahoo.co.uk>
2023-07-13 22:25:29 +0100

    Initial configuration of Vitest with react-testing-library and jest-dom

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     // By extending from a plugin config, we can get recommended rules without having to add them manually.
     'eslint:recommended',
