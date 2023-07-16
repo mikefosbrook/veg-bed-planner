f00540b1919202938f0b4c86ba10ebf87d2fb6fe
Mike Fosbrook <mikefosbrook@yahoo.co.uk>
2023-07-16 22:16:25 +0100

    Updates Home tests
    - Adds deleteBed handler to intercept DELETE request
    - Adds test for deleting a bed
    - Adds test for clicking a bed and navigating to the bed detail page
    - Adds test to check a user can navigate to the create bed page after clicking 'Add a new bed'

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,10 +1,16 @@
 import { rest } from 'msw';
 import bedMockData from './bed-mock-data';
 
-function getPosts() {
+function getBeds() {
   return rest.get('http://localhost:4000/beds/', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(bedMockData));
   });
 }
 
-export const handlers = [getPosts()];
+function deleteBed() {
+  return rest.delete('http://localhost:4000/beds/:id', (req, res, ctx) => {
+    return res(ctx.status(200));
+  });
+}
+
+export const handlers = [getBeds(), deleteBed()];
